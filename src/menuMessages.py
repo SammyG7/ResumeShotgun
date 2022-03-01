d4c8ebe090110cb09d67480c872cdc6ab7070cc4
jamesong <jamesong@mcmaster.ca>
2022-03-01 14:26:23 -0500

    tweaked doxygen, added waitforuser

--- src/menuMessages.py
+++ src/menuMessages.py
@@ -1,11 +1,8 @@
 from os import name, system
 
-## Widths are 30 characters max
+# Widths are 30 characters max
 
-
-##
-#  @brief Displays text for main menu options
-# 
+## @brief Displays text for main menu options
 def displayMenuMain():
     print("==============================\n" +
           "        RESUME SHOTGUN        \n" +
@@ -20,10 +17,8 @@ def displayMenuMain():
           " 0) Save and Start \n" +
           "\n")
     
-##
-#  @brief Displays text for resume menu options
+## @brief Displays text for resume menu options
 #  @param path String for current path to resume file
-# 
 def displayMenuResume(path):
     if path == "": path = "N/A"
     print("==============================\n" +
@@ -39,10 +34,8 @@ def displayMenuResume(path):
           " 0) Go back \n" +
           "\n")
 
-##
-#  @brief Displays text for keywords menu options
+## @brief Displays text for keywords menu options
 #  @param keywords List of strings of current keywords
-# 
 def displayMenuKeywords(keywords):
     if len(keywords) == 0: keywords = "N/A"
     print("==============================\n" +
@@ -62,10 +55,8 @@ def displayMenuKeywords(keywords):
           " 0) Go back \n" +
           "\n")
 
-##
-#  @brief Displays text for sites menu options
+## @brief Displays text for sites menu options
 #  @param site current site
-# 
 def displayMenuSites(site):
     print("==============================\n" +
           "        RESUME SHOTGUN        \n" +
@@ -83,10 +74,8 @@ def displayMenuSites(site):
           " 0) Go back \n" +
           "\n")
 
-##
-#  @brief Displays text for unimplemented menu
+## @brief Displays text for unimplemented menu
 #  @param menu Optional string for menu location (default "??")
-# 
 def displayMenuPlaceholder(menu = "??"):
     print("==============================\n" +
           "        RESUME SHOTGUN        \n" +
@@ -100,22 +89,17 @@ def displayMenuPlaceholder(menu = "??"):
           "Returning to main menu... \n" +
           "\n")
 
-## 
-#  @brief Clears console screen
-# 
+## @brief Clears console screen
 def clearScreen():
     system("cls" if name == "nt" else "clear")
 
-## 
-#  @brief Adds newlines to a string or combines a list of values
-#  with newlines such that it does not exceed a given width
-#  @details Each new line is padded with a space for optimal asthetics,
-#  that space character IS included in the width
+## @brief Adds newlines to a string or combines a list of values with newlines such that 
+#  it does not exceed a given width
+#  @details Each new line is padded with a space for optimal asthetics, that space character 
+#  IS included in the width
 #  @param raw String or list to wrap
-#  @param width Optional integer for maximum width allowed (default 29)
-#  @return String with no line longer than the given width in characters
-#  (if possible)
-# 
+#  @param width (optional) integer for maximum width allowed
+#  @return String with no line longer than the given width in characters (if possible)
 def wrappedString(raw, width = 30):
     out = " "
     lineLength = 1
@@ -147,13 +131,8 @@ def wrappedString(raw, width = 30):
                "but is simply here to prevent an error " +
                "that would happen if it didn't")
                 
-        
-
-##
-#  @brief Displays error messages
-#  @param error Optional error message type to display
-#  (defaults to general message)
-#
+## @brief Displays error messages
+#  @param error (optional) error message type to display
 def displayError(error = "general"):
     msgs = {
         "input": "Invalid input selected.",
@@ -163,5 +142,7 @@ def displayError(error = "general"):
         }
     print(wrappedString("!! " + msgs[error]) + "\n")
 
-
+## @brief Prompts user to confirm before continuing
+def waitForUser():
+    input("([Enter] to continue)\n")
         
