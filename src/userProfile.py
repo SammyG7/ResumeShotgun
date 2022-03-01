344245794b135625f8948decf6fc957677a682dc
jamesong <jamesong@mcmaster.ca>
2022-03-01 16:26:31 -0500

    added rest of basic functions for class

--- src/userProfile.py
+++ src/userProfile.py
@@ -4,7 +4,7 @@
 #  @date Mar 1, 2022
 
 from menuMessages import wrappedString as wS, waitForUser
-from os.path import isfile
+from os.path import isfile, join
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
@@ -13,15 +13,15 @@ except ImportError:
 ## @brief this class represents the preferences of a user for job searching 
 class userProfile:
 
-    ## @brief constructor for userProfile
-    #  @details sets default values for profile
+    ## @brief Constructor for userProfile
+    #  @details Sets default values for profile
     def __init__(self):
         self.__resumePath = ""
         self.__keywords = []
         self.__site = "glassdoor"
 
-    ## @brief method attempts to load values from the given file to itself
-    #  @param file (optional) a path to the saved profile
+    ## @brief Method attempts to load values from the given file to itself
+    #  @param file (optional) A path to the saved profile
     def loadProfile(self, file = "profile.yaml"):
         if isfile(file):
             print(wS("Loading profile from " + file + "...") + "\n")
@@ -57,9 +57,9 @@ class userProfile:
             print(wS("Profile not found, using default values.") + "\n")
             waitForUser()
 
-    ## @brief method attempts to save its variable values to the given file
-    #  @details will overwrite the file if it exists already
-    #  @param file (optional) a path to the profile to write 
+    ## @brief Method attempts to save its variable values to the given file
+    #  @details Will overwrite the file if it exists already
+    #  @param file (optional) A string indicating a path to the profile to write 
     def saveProfile(self, file = "profile.yaml"):
         print(wS("Saving profile to " file "...") + "\n")
         profileFile = open(file, "w")
@@ -72,67 +72,53 @@ class userProfile:
         print("Saved! \n")
         waitForUser()
 
-    ## @brief a method for getting the front top left corner of the box
-    #  @return a tuple of three real numbers indicating the location of the front top left corner
-    def getFrontTopLeftCorner(self):
-        return self.__frontTopLeftCorner
+    # ---------- getters ----------
 
-    ## @brief a method for setting the front top left corner of the box
-    #  @param newCorner (x, y, z) where x,y,z are real numbers indicating coordinate of the front top left corner
-    def setFrontTopLeftCorner(self, newCorner):
-        self.__frontTopLeftCorner = newCorner
+    ## @brief Method gets path to resume file
+    #  @return String indicating path to resume file
+    def getResumePath(self):
+        return self.__resumePath
 
+    ## @brief Method gets keywords to use in searching
+    #  @return List of strings indicating keywords
+    def getKeywords(self):
+        return self.__keywords
 
-    ## @brief a method for getting the dimensions of the box
-    #  @return a tuple of three real numbers indicating the width, height and depth of the box
-    def getDimensions(self):
-        return self.__dimensions
+    ## @brief Method gets site that will be scraped
+    #  @return String indicating name of site
+    def getResumePath(self):
+        return self.__site
 
-    ## @brief a method for getting the dimensions of the box
-    #  @param newSize (width, height, depth) where x,y,z are real numbers and
-    #  x,y,z are greater than or equal to 0, indicating side lengths
-    def setDimensions(self, newSize):
-        self.__dimensions = newSize
+    # ---------- setters ----------
 
-    ## @brief a method for getting the dimensions of a face
-    #  @param face an integer corresponding to the face to get dimensions from; face is one of [0, 1, 2]
-    #  @return a tuple of two real numbers indicating the dimensions that define the specified face
-    #  @throws ValueError if the face specified does not map to the cube
-    def getDimensionsOfFace(self, face):
-        if face == Box3D.FRONT:
-            width,height = self.__dimensions[0],self.__dimensions[1] # w,h
-        elif face == Box3D.TOP:
-            width,height = self.__dimensions[0],self.__dimensions[2] # w,d
-        elif face == Box3D.SIDE:
-            width,height = self.__dimensions[1],self.__dimensions[2] # h,d
-        else:
-            raise ValueError("Invalid `face` argument")
-        return width,height
+    ## @brief Method tries to set path to resume file
+    #  @details Method also checks if file exists, and if it does not it will not change it
+    #  @param file A string indicating a path to a resume file
+    #  @param appendWorking (optional) Append the working directory to the file if applicable
+    #  @return Boolean True if file exists and successfully changes, False if not
+    def setResumePath(self, file, appendWorking = True):
+        if isfile(file):
+            if appendWorking and isfile(join(getcwd(), file)):
+                self.__resumePath = join(getcwd(), file)
+            else: 
+                self.__resumePath = file
 
-    ## @brief a method for getting the area of a face
-    #  @param face an integer corresponding to the face to get dimensions from; face is one of [0, 1, 2]
-    #  @return a real number indicating the area of a face
-    def getAreaOfFace(self, face):
-        width,height = self.getDimensionsOfFace(face)
-        return width * height
+    ## @brief Method sets keywords
+    #  @param keywords A list of strings used as keywords
+    #  @param toggleMode (optional) If true, instead of overwriting the class varible with 
+    #  the parameter, it will add words to the class varable from the parameter that are not 
+    #  there and remove ones that are
+    def setKeywords(self, keywords, toggleMode = False):
+        if toggleMode:
+            for word in keywords:
+                if word.replace(" ", "") != "":
+                    word = word.lower()
+                    try: self.__keywords.remove(word)
+                    except ValueError: self.__keywords.append(word)
+        else: 
+            self.__keywords = keywords
 
-    ## @brief a method for getting the perimeter of a face
-    #  @param face an integer corresponding to the face to get dimensions from; face is one of [0, 1, 2]
-    #  @return a real number indicating the perimeter of a face
-    def getPerimeterOfFace(self, face):
-        width,height = self.getDimensionsOfFace(face)
-        return 2 * (width + height)
+    ## @brief CHECK
+    def setSite(self, site):
+        self.__site = site
 
-    ## @brief a method for getting the volume of the box
-    #  @return a real number indicating the volume of the box
-    def getVolume(self):
-        width,height,depth = self.__dimensions
-        return width * height * depth
-
-    ## @brief a method for getting the surface area of the box
-    #  @return a real number indicating the surface area of the box
-    def getSurfaceArea(self):
-        surfaceArea = 0
-        for face in (Box3D.FRONT, Box3D.TOP, Box3D.SIDE):
-            surfaceArea += (2 * self.getAreaOfFace(face))
-        return surfaceArea
