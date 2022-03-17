4eb31f25b3ab7eb2fc057258223d009b23e61428
Jeremy Langner <langnerj@mcmaster.ca>
2022-03-17 11:08:36 -0400

    Added doxygen comments

--- src/get_links_indeed.py
+++ src/get_links_indeed.py
@@ -1,3 +1,8 @@
+## @file get_links_indeed.py
+#  @author Samuel Gorman
+#  @brief Module that signs into Indeed website.
+#  @date March 17, 2022
+
 # selenium stup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -22,6 +27,9 @@ import time # to sleep
 ##}
 ##
 
+## @brief Opens indeed.com/ and checks if user is logged in, otherwise calls function to auto login.
+#  @param driver a Selenium object utilized to navigate the page.
+#  @return boolean representing sign in has been prompted or user is already signed in.
 def login(driver):
     driver.get('https://ca.indeed.com/')
     
@@ -39,6 +47,8 @@ def login(driver):
 
     return True
 
+## @brief Opens Indeed login page and inputs email and password then signs in.
+#  @param driver a Selenium object utilized to navigate the page.
 def navigateToLogin(driver):
     driver.get("https://secure.indeed.com/account/login?hl=en_CA&amp;co=CA&amp;continue=https%3A%2F%2Fca.indeed.com%2Fjobs%3Fq%3Dmaterials%2Bengineer%26l%3DHamilton%252C%2BON%26ts%3D1646004111972%26pts%3D1641679429865%26rq%3D1%26rsIdx%3D1&amp;tmpl=desktop&amp;service=my&amp;from=gnav-util-jobsearch--jasx")
     email = driver.find_element_by_xpath("//*[@id='ifl-InputFormField-3']")
@@ -145,8 +155,10 @@ def navigateToLogin(driver):
 ##    # convert to a set to eliminate duplicates
 ##    return set(allFixedLinks)
 
-# 'main' method to iterate through all pages and aggregate URLs
+## @brief Instantiates a Selenium chrome driver with executable path to the chrome driver lcoation then calls login()
 def getURLs():
+    # 'main' method to iterate through all pages and aggregate URLs
+
     driver = webdriver.Chrome(executable_path='./chromedriver')
     success = login(driver)
     time.sleep(100)
